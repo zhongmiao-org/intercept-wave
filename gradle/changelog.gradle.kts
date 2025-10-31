ee1f450e06c232e7ee577205bb2bf459274aa258
ark-65 <liuwufangzhou@vip.qq.com>
2025-10-31 14:34:20 +0800

    fix: build

--- gradle/changelog.gradle.kts
+++ gradle/changelog.gradle.kts
@@ -1,6 +1,12 @@
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-changelog {
-    groups.empty()
-    repositoryUrl = providers.gradleProperty("pluginRepositoryUrl")
-}
+// Use explicit extension configuration to avoid unresolved DSL in applied scripts
+import org.jetbrains.changelog.ChangelogPluginExtension
 
+val repoUrl = providers.gradleProperty("pluginRepositoryUrl")
+
+extensions.configure<ChangelogPluginExtension> {
+    // Equivalent to `groups.empty()` in the DSL
+    groups.set(emptyList())
+    // Use Gradle Property API
+    repositoryUrl.set(repoUrl)
+}
